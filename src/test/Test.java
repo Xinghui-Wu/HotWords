782b564da23ea23c71f4a01c5bc1774dc71c2244
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-02-23 00:26:35 +0800

    Write the main function for test.

---
+++ src/test/Test.java
@@ -0,0 +1,40 @@
+package test;
+
+import dictionary.Dictionary;
+import nlp.DAG;
+import nlp.NLP;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//系统启动初始化时预加载汉语词典
+//		Dictionary.initialize();
+
+//		String weibo = "北京演播室的嘉宾，特约时事评论员于强认为，无人参选、选区艰困， 国民党才有理由开启征召。韩国瑜的政绩及声势是决定是否参选的重要因素，若民进党继续集全党之力“打韩” ，韩国瑜被征召可能性将变大...";
+//		String weibo = "去西安交通大学学习";
+//		NLP.nlp(weibo);
+//
+//		DAG.outputWordList(0);
+//		NLP.outputArrayList();
+//		NLP.outputHashMap();
+//		NLP.outputPropertyCount();
+		
+		
+		
+//		double idf = Dictionary.getIDF("@@@");
+//		System.out.println("idf = " + idf);
+		
+//		Word word = Dictionary.search("西北工业");
+//		if(word == null)
+//			System.out.println("null");
+//		else
+//			System.out.println(word.getWord() + "    &    id = " + word.getId());
+		
+//		long start = System.nanoTime();
+//		long end = System.nanoTime();
+//		double duration = (end-start) / 1000000.0;
+//		System.out.println("duration = " + duration);
+	}
+
+}
